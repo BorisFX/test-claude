c044b8fc8724395ed8eea8dde364c83a45ed5105
Oleg Matyakubov <gordon.clop@gmail.com>
2024-12-12 16:20:02 +0700

    Initial commit with Telegram bot project

---
+++ Program.cs
@@ -0,0 +1,71 @@
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var botClient = new TelegramBotClient("YOUR_BOT_TOKEN_HERE");
+
+        using CancellationTokenSource cts = new();
+
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() 
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        var me = await botClient.GetMeAsync();
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+
+        cts.Cancel();
+    }
+
+    static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Message is not { } message)
+            return;
+        if (message.Text is not { } messageText)
+            return;
+
+        var chatId = message.Chat.Id;
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+        var replyText = messageText.ToLower() switch
+        {
+            "/start" => "Привет! Я тестовый бот. Используйте команды:\n/help - список команд",
+            "/help" => "Доступные команды:\n/start - начать\n/help - помощь\n/echo [текст] - повторить текст",
+            var text when text.StartsWith("/echo ") => text[6..],
+            _ => "Используйте /help для просмотра команд"
+        };
+
+        Message sentMessage = await botClient.SendMessageAsync(
+            chatId: chatId,
+            text: replyText,
+            cancellationToken: cancellationToken);
+    }
+
+    static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
+}
